04303c7e2e9c4b2557fd32f9b7fe093dc454dae9
Robel <155951971+Robiin28@users.noreply.github.com>
2026-01-29 00:45:34 +0300

    git redirection fixing by adding base on the nav url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  base:"meli-static-site",
+  base: "/meli-static-site/",
+  trailingSlash: "always",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
